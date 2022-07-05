5fa14ea537eda86bf97f1bd0833a5ef966159a7b
live-young <61771639+live-young@users.noreply.github.com>
2022-07-05 10:57:46 +0530

    creating common functionlity method

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 	static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-		return (checkIsOk(chargeRate, 0, 0.8, "Charge Rate") 
-				||checkIsOk(soc, 20, 80, "State of Charge") 
-				|| checkIsOk(temperature, 0, 45, "Temperature"));
+		return (checkIsOk(temperature, 0, 45, "Temperature")
+				&& checkIsOk(soc, 20, 80, "State of Charge") 
+				&& checkIsOk(chargeRate, 0, 0.8, "Charge Rate"));
 	}
 	
 	static boolean checkIsOk(float parameter, float lowerLimit , double upperLimit, String checkName) {
